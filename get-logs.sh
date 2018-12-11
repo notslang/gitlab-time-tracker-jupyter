2cd9eb54077f726a59fc07d8eab1da1234d956f4
Sean Lang <slang800@gmail.com>
2018-12-11 15:16:29 -0600

    add script for getting logs with gtt

---
+++ get-logs.sh
@@ -0,0 +1,2 @@
+gtt report --from=2018-11-24 --report=records --output=csv --no_headlines -c --time_format="[%sign][%hours_overall]" msoe.edu/sdl/create-institute/backend --file="./gitlab-time-tracker-backend.csv"
+gtt report --from=2018-11-24 --report=records --output=csv --no_headlines -c --time_format="[%sign][%hours_overall]" msoe.edu/sdl/create-institute/frontend --file="./gitlab-time-tracker-frontend.csv"
