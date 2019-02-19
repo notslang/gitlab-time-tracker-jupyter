03b662f890559a4951878ad43722183fb5c6d1b4
Sean Lang <slang800@gmail.com>
2019-02-19 13:16:09 -0600

    update get-logs script

--- get-logs.sh
+++ get-logs.sh
@@ -1,2 +1,21 @@
-gtt report --from=2018-11-24 --report=records --output=csv --no_headlines -c --time_format="[%sign][%hours_overall]" msoe.edu/sdl/create-institute/backend --file="./gitlab-time-tracker-backend.csv"
-gtt report --from=2018-11-24 --report=records --output=csv --no_headlines -c --time_format="[%sign][%hours_overall]" msoe.edu/sdl/create-institute/frontend --file="./gitlab-time-tracker-frontend.csv"
+gtt report \
+    --from=2019-01-29 \
+    --to=2019-02-19 \
+    --report=records \
+    --output=csv \
+    --no_headlines \
+    -c \
+    --time_format="[%sign][%hours_overall]" \
+    msoe.edu/sdl/create-institute/backend \
+    --file="./gitlab-time-tracker-backend.csv"
+
+gtt report \
+    --from=2019-01-29 \
+    --to=2019-02-19 \
+    --report=records \
+    --output=csv \
+    --no_headlines \
+    -c \
+    --time_format="[%sign][%hours_overall]" \
+    msoe.edu/sdl/create-institute/frontend \
+    --file="./gitlab-time-tracker-frontend.csv"
